25a409921d8e67a9c375cf6d698725977536934a
looping <www.looping@gmail.com>
2013-05-30 23:35:36 +0800

    db user foreignkey

--- m.py
+++ m.py
@@ -9,6 +9,7 @@ class BaseModel(Model):
 class Blog(BaseModel):
 	content = CharField()
 	title = CharField()
+	author = ForeignKeyField(User)	
 	def __unicode__(self):
 		return self.title
 class User(BaseModel):
@@ -17,6 +18,8 @@ class User(BaseModel):
 	last_name = CharField()
 	password = CharField()
 	last_login = DateField()
+	email = CharField()
+	first_login = DateField()
 
 
 def create_tables():
