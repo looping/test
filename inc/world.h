07ed60bcc9d30b1efeea1c79571d423f0d44f7ea
looping <xulp@outlook.com>
2013-07-22 23:27:07 +0800

    World is running!

--- inc/world.h
+++ inc/world.h
@@ -1,1 +1,7 @@
+struct World{
+	int point ;
+};
+typedef struct World World;
 void world_start(void);
+World init_world();
+void run(World world);
--- src/world.c
+++ src/world.c
@@ -1,4 +1,13 @@
 #include<world.h>
+World init_world(void){
+	World world;
+	return world;
+}
+void run(World world){
+	while(1){
+		printf("\nWorld is running!");
+	}
+}
 void world_start(void){
-	while(1)printf("hahahaha");
+	run(init_world());
 }
